ad00364c541c6a9f211358b46c1f7576b67b2e5b
Ninja <72120564+Nowfel1@users.noreply.github.com>
2022-10-26 18:14:46 +0600

    Update arbitrum_settings.bash

--- 04-my_token/scripts/arbitrum_settings.bash
+++ 04-my_token/scripts/arbitrum_settings.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-brownie networks add Arbitrum arbitrum_rinkeby host='https://sleek-dimensional-replica.arbitrum-goerli.discover.quiknode.pro/a290bffae7fcdbfe5d86e7ffe182c7088305c58b/' name='Donk DAO' chainid=421611 explorer='https://goerli-rollup-explorer.arbitrum.io/#/'
+brownie networks add Arbitrum arbitrum_rinkeby host='https://sleek-dimensional-replica.arbitrum-goerli.discover.quiknode.pro/a290bffae7fcdbfe5d86e7ffe182c7088305c58b/' name='Nitro Goerli Rollup Testnet' chainid=421613 explorer='https://goerli-rollup-explorer.arbitrum.io/#/'
